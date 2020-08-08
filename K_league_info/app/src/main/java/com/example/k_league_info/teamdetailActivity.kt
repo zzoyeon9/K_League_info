a64a640892827598301d0e90db5e47cf7da85b20
seongwon-bae <bsy8854@naver.com>
2020-08-08 18:03:58 +0900

    Player 화면 연결

--- K_league_info/app/src/main/java/com/example/k_league_info/teamdetailActivity.kt
+++ K_league_info/app/src/main/java/com/example/k_league_info/teamdetailActivity.kt
@@ -1,13 +1,20 @@
 package com.example.k_league_info
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_teamdetail.*
 
 class teamdetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_teamdetail)
+
+        tmp_buttton.setOnClickListener{
+            val nextIntent = Intent(this, Player::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
