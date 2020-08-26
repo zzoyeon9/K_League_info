8f5d34d0b5c659b3f7774108aaa2f39239a4b98d
bvon95 <bvon95@metanet.co.kr>
2020-08-26 15:35:48 +0900

    인트로 페이지 완성

---
+++ K_league_info/app/src/main/java/com/example/k_league_info/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.k_league_info
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long=1000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
