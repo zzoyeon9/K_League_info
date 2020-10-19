940178adaf9cc4d62572708581f4b9fd41e0baa2
y2sec <bae3835@gmail.com>
2020-10-19 20:37:47 +0900

    viewModel and community remake

--- K_league_info/app/src/main/java/com/example/k_league_info/ui/score/ScoreFragment.kt
+++ K_league_info/app/src/main/java/com/example/k_league_info/ui/score/ScoreFragment.kt
@@ -1,16 +1,14 @@
 package com.example.k_league_info.ui.score
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.k_league_info.R
-import com.example.k_league_info.ScoredetailActivity
+
+import kotlinx.android.synthetic.main.fragment_score.*
 
 class ScoreFragment : Fragment() {
 
@@ -28,30 +26,18 @@ class ScoreFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val rootView = inflater.inflate(R.layout.fragment_score, container, false)
-        val rootView2 = inflater.inflate(R.layout.item_score, container, false)
-
-        val score_Adapter = ScoreAdapter(requireContext(), boardList)
-        val myrecycler = rootView.findViewById(R.id.score_recyclerView!!) as RecyclerView
-        myrecycler.layoutManager = LinearLayoutManager(requireContext())
-        myrecycler.adapter = score_Adapter
-
-
-        var scoreCard = rootView2.findViewById<CardView>(R.id.score_card)
-        scoreCard.setOnClickListener {
-            activity?.let {
-                val nextIntent = Intent(context, ScoredetailActivity::class.java)
-                startActivity(nextIntent)
-            }
-        }
-
-
-        return rootView2
+        return inflater.inflate(R.layout.fragment_score, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        val scoreAdapter = activity?.let { ScoreAdapter(it, boardList) }
+        score_recyclerView.adapter = scoreAdapter
+
+        val lm = LinearLayoutManager(activity)
+        score_recyclerView.layoutManager = lm
+        score_recyclerView.setHasFixedSize(true)
     }
 }
