8f606773c6783be58c3d6388f939779173346728
y2sec <bae3835@gmail.com>
2020-10-22 23:58:10 +0900

    SplashActivity에서 데이터 받아와 MainActivity로 보내기 MainActivity에서는 MyViewModel에 저장

--- K_league_info/app/src/main/java/com/example/k_league_info/SplashActivity.kt
+++ K_league_info/app/src/main/java/com/example/k_league_info/SplashActivity.kt
@@ -2,23 +2,47 @@ package com.example.k_league_info
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.k_league_info.ui.community.CommunityBoard
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.json.JSONArray
 
 class SplashActivity : AppCompatActivity() {
-    private val SPLASH_TIME_OUT:Long=1000
-
     private var retrofitClient = RetrofitClient()
     private val retrofit = retrofitClient.getInstance()
-    private val api: RetrofitNetwork = retrofit.create(RetrofitNetwork::class.java)
-
+    private val restAPI: RetrofitNetwork = retrofit.create(RetrofitNetwork::class.java)
+    private var communityList = ArrayList<CommunityBoard>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+
+        val thread = BringDataThread()
+        thread.start()
+
+    }
+    inner class BringDataThread :Thread() {
+        override fun run() {
+            val loading : TextView = findViewById(R.id.loading)
+            loading.text = "community Loading..."
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val gson = GsonBuilder().create()
+            val communityJSONArray = JSONArray(restAPI.getCommunity().execute().body().toString())
+            for (i in 0 until communityJSONArray.length()) {
+                val board = gson.fromJson(
+                    communityJSONArray.getJSONObject(i).toString(),
+                    CommunityBoard::class.java
+                )
+                communityList.add(board)
+            }
+            intent.putExtra("community", communityList)
+
+
+            loading.text = "finish!!"
+            startActivity(intent)
             finish()
-        }, SPLASH_TIME_OUT)
+        }
     }
 }
--- K_league_info/app/src/main/java/com/example/k_league_info/ui/community/PostWrite.kt
+++ K_league_info/app/src/main/java/com/example/k_league_info/ui/community/PostWrite.kt
@@ -1,16 +1,12 @@
 package com.example.k_league_info.ui.community
 
-import android.app.ProgressDialog
 import android.os.Bundle
-import android.util.Log
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.k_league_info.R
 import com.example.k_league_info.RetrofitClient
 import com.example.k_league_info.RetrofitNetwork
 import kotlinx.android.synthetic.main.activity_post_write.*
-import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,7 +14,7 @@ import retrofit2.Response
 class PostWrite : AppCompatActivity() {
     private var retrofitClient = RetrofitClient()
     private val retrofit = retrofitClient.getInstance()
-    private val api: RetrofitNetwork = retrofit.create(RetrofitNetwork::class.java)
+    private val restAPI: RetrofitNetwork = retrofit.create(RetrofitNetwork::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +22,9 @@ class PostWrite : AppCompatActivity() {
 
         ok.setOnClickListener {
             Toast.makeText(applicationContext,"글 작성중입니다.",Toast.LENGTH_LONG).show()
-            val json = JSONObject()
-            json.put("title", titles.text.toString())
-            json.put("content", content.text.toString())
-            val cb = CommunityBoard(0,titles.text.toString(),content.text.toString())
-            api.setPost(cb).enqueue(object : Callback<Void>{
+
+            val cb = CommunityBoard(0, titles.text.toString(), content.text.toString(), passwd.text.toString())
+            restAPI.postCommunity(cb).enqueue(object : Callback<Void>{
                 override fun onFailure(call: Call<Void>, t: Throwable) {
                     Toast.makeText(applicationContext,"실패하였습니다.",Toast.LENGTH_LONG).show()
                 }
